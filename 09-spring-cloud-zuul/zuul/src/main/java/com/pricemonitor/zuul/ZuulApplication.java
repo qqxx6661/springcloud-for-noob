0933380abedd1867e180f28524770add28ade79a
YangZhendong <404013419@qq.com>
2019-03-23 19:36:05 +0800

    更新教程代码：09

---
+++ 09-spring-cloud-zuul/zuul/src/main/java/com/pricemonitor/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.pricemonitor.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
+    }
+}
