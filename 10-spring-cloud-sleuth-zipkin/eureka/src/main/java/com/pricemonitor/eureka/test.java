a7b8561e4e92b89f6654508d2032d7c8157cf26a
270463736@qq.com <270463736@qq.com>
2020-02-25 23:51:33 +0800

    测试提交

---
+++ 10-spring-cloud-sleuth-zipkin/eureka/src/main/java/com/pricemonitor/eureka/test.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: test
+ * Author:   27046
+ * Date:     2020/2/25 21:45
+ * Description: test class
+ * History:
+ * <p>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.pricemonitor.eureka;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈test class〉
+ *
+ *@author 27046
+ *@create 2020/2/25
+ *@since 1.0.0
+ */
+public class test {
+
+    public int test(){
+        /* * 功能描述:
+         * 〈〉
+         *
+         * @param
+         *@return:int
+         *@since: 1.0.0
+         *@Author:27046
+         *@Date: 2020/2/25 22:38
+         */
+        return 5;
+    }
+}
