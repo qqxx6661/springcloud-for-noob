3d691e06868b57ab322c4248bdb5eb09bd29a525
270463736@qq.com <270463736@qq.com>
2020-02-26 00:44:30 +0800

    测试提交2

---
+++ 10-spring-cloud-sleuth-zipkin/eureka/src/main/java/com/pricemonitor/eureka/Mybranch2.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: Mybranch2
+ * Author:   27046
+ * Date:     2020/2/26 0:14
+ * Description: tttest
+ * History:
+ * <p>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.pricemonitor.eureka;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈tttest〉
+ *
+ *@author 27046
+ *@create 2020/2/26
+ *@since 1.0.0
+ */
+public class Mybranch2 {
+    public void a(){
+
+    }
+}
